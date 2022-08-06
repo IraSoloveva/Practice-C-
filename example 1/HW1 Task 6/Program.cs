23de21563956eb0f337af0157774246f0f5c7cba
soloveva <solovevaia@susu.ru>
2022-08-06 15:24:52 +0500

    task 6

--- example 1/HW1 Task 6/Program.cs
+++ example 1/HW1 Task 6/Program.cs
@@ -1,21 +1,16 @@
 ﻿
+Console.WriteLine("Введеите число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    
+    int number2 =2;
+    int r = (number1 % number2);
 
-Console.WriteLine("Введеите 1 число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введеите 2 число");
-int number2 = Convert.ToInt32(Console.ReadLine());
+    if (r == 0)
+    {
+        Console.WriteLine("Да");
 
-if (number1 > number2)
-{
-    Console.Write("Большее число:");
-    Console.WriteLine(number1);
-    Console.Write("Меньшее число:");
-    Console.WriteLine(number2);
-}
-else
-{
-    Console.Write("Большее число:");
-    Console.WriteLine(number2);
-    Console.Write("Меньшее число:");
-    Console.WriteLine(number1);
-}
+    }
+    else 
+    {
+         Console.WriteLine("Нет");
+    }
