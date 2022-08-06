f1f006852de0be6a173598994b7802a4b39cf382
soloveva <solovevaia@susu.ru>
2022-08-06 15:09:00 +0500

    task 6

---
+++ example 1/HW1 Task 6/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("Введеите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введеите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(number1);
+    Console.Write("Меньшее число:");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(number2);
+    Console.Write("Меньшее число:");
+    Console.WriteLine(number1);
+}
