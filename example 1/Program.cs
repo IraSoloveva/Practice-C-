65cb4559f239f9833004d9417fb47415c2d0fb8c
soloveva <solovevaia@susu.ru>
2022-08-06 12:40:27 +0500

    math operators

---
+++ example 1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Введите оператор");
+    String Operator = Console.ReadLine();
+    if (Operator == "end")
+    {
+        break;
+    }
+    Console.WriteLine("Введеите 1 число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введеите 2 число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (Operator == "+")
+    {
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Operator == "/")
+    {
+        Console.WriteLine(number1 / number2);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
