46a9796737909f9ef063d3a28a89103a1f93bd97
soloveva <solovevaia@susu.ru>
2022-08-06 15:49:54 +0500

    task 8

---
+++ example 1/HW1 Task 8/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Console.WriteLine("Введеите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = 2;
+int count = 1;
+
+while (count < number1 + 1)
+{
+    int r = (count % number2);
+
+    if (r == 0)
+    {
+        Console.Write(count);
+    }
+  count = count +1;
+}
