17d39db406fb29e2becbfad18df9c9c0e17f7526
soloveva <solovevaia@susu.ru>
2022-08-09 16:44:22 +0500

    task 6

--- example 1/HW1 Task 6/Program.cs
+++ example 1/HW1 Task 6/Program.cs
@@ -1,16 +1,13 @@
-﻿
-Console.WriteLine("Введеите число");
-    int number1 = Convert.ToInt32(Console.ReadLine());
-    
-    int number2 =2;
-    int r = (number1 % number2);
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+int r = (number1 % number2);
+if (r == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
 
-    if (r == 0)
-    {
-        Console.WriteLine("Да");
-
-    }
-    else 
-    {
-         Console.WriteLine("Нет");
-    }
